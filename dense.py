de209e9d378ff86ddeb5ef92ecec2b766c3847e9
Nazar Hasanov <nazarhasanov@ip-10-111-66-73.ap-southeast-1.compute.internal>
2024-05-27 14:59:11 +0800

    initial commit

---
+++ dense.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+
+# Dense layer from scratch
+
+class MyDenseLayer (tf.keras.layers.Layer):
+    def __init__(self, input_dim, output_dim):
+        super (MyDenseLayer, self).__init__()
+        
+        # Initialize weights and bias
+        self.W = self.add_weight ([input_dim, output_dim])
+        self.b = self.add_weight ([1, output_dim])
+    
+    def call (self, inputs):    
+        # Forward propagate the inputs
+        z = tf. matmul (inputs, self.W) + self.b
+    
+        # Feed through a non-linear activation
+        output = tf.math.sigmoid(z)
+        return output
